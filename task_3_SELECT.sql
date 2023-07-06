f503d49603f0018bfd6074bacc199928d1223b7b
Alexander Godov <godovav@oiate.ru>
2023-07-06 21:15:13 +0300

    Edit SELECT and INSERT

--- task_3_SELECT.sql
+++ task_3_SELECT.sql
@@ -2,7 +2,7 @@ SELECT name_of_track, duration FROM track
 WHERE duration = (SELECT MAX(duration) FROM track);
 
 SELECT name_of_track, duration FROM track
-WHERE duration >= 3.5;
+WHERE duration >= 210;
 
 SELECT title, release_year FROM collection
 WHERE release_year BETWEEN 2018 AND 2020;
@@ -11,24 +11,26 @@ SELECT alias_or_band FROM musician
 WHERE alias_or_band NOT LIKE '% %';
 
 SELECT name_of_track FROM track
-WHERE UPPER(name_of_track) LIKE '%MY%' OR UPPER(name_of_track) LIKE '%МОЙ%';
+WHERE STRING_TO_ARRAY(LOWER(name_of_track), ' ') && ARRAY['my', 'мой'];
 
 SELECT * FROM (SELECT genre_id, COUNT(musician_id) FROM genre_musician
 GROUP BY genre_id) t JOIN genre g ON g.genre_id = t.genre_id;
 
 SELECT COUNT(a.title) FROM album a
 JOIN track t ON t.album_id = a.album_id
-WHERE a.release_year BETWEEN 2019 AND 2020
-GROUP BY a.title;
+WHERE a.release_year BETWEEN 2019 AND 2020;
 
 SELECT * FROM (SELECT t.album_id, AVG(t.duration) FROM track t
 JOIN album a ON a.album_id = t.album_id
 GROUP BY t.album_id) t JOIN album a ON a.album_id = t.album_id;
 
-SELECT m.alias_or_band FROM musician m
-JOIN (SELECT * FROM album_musician am 
-JOIN album a ON a.album_id = am.album_id
-WHERE a.release_year != 2020) t ON m.musician_id = t.musician_id;
+SELECT alias_or_band FROM musician
+WHERE alias_or_band NOT IN (
+	SELECT m.alias_or_band FROM musician m
+	JOIN album_musician am ON m.musician_id = am.musician_id
+	JOIN album a ON am.album_id = a.album_id
+	WHERE a.release_year = 2020
+);
 
 SELECT c.title FROM collection c
 JOIN (SELECT DISTINCT ct.collection_id FROM collection_track ct

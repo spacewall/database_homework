50e4d12547a7536ae629000075a8fd112311bb5d
Alexander Godov <godovav@oiate.ru>
2023-07-18 15:00:55 +0300

    ORM learing

--- models.py
+++ models.py
@@ -12,13 +12,26 @@ class Course(Base):
 
     homework = relationship("Homework", back_populates="course")
 
+    def __str__(self):
+        return f'Course {self.id}: {self.name}'
+    
 
 class Homework(Base):
     __tablename__ = "homework"
 
     id = sql.Column(sql.Integer, primary_key=True)
-    number = sql.Column(sql.Integer, nulable=False)
+    number = sql.Column(sql.Integer, nullable=False)
     description = sql.Column(sql.Text, nullable=False)
-    curse_id = sql.Column(sql.Integer, sql.ForeignKey("course.id"), nullable=False)
+    course_id = sql.Column(sql.Integer, sql.ForeignKey("course.id"), nullable=False)
 
-    course = relationship(Course, back_populates="homeworks")
+    # course = relationship(Course, back_populates="homeworks")
+    course = relationship(Course, backref="homeworks")
+
+    def __str__(self):
+        return f"Homework {self.id}: ({self.number}, {self.description}, {self.course})"
+    
+
+
+def create_tables(engine):
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
--- orm_examples.py
+++ orm_examples.py
@@ -1,14 +1,54 @@
 import sqlalchemy
+from models import create_tables, Course, Homework
 from sqlalchemy.orm import sessionmaker
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 LOGIN = config["username"]
 PASSWORD = config["password"]
-data_source_name = f'postgresql://{LOGIN}:{PASSWORD}@localhost:5432/postgres'
-
+DATABASE = config["database"]
+data_source_name = f'postgresql://{LOGIN}:{PASSWORD}@localhost:5432/{DATABASE}'
 engine = sqlalchemy.create_engine(data_source_name)
+
+create_tables(engine)
+
 Session = sessionmaker(engine)
 session = Session()
+
+course_1 = Course(name='Python')
+
+session.add(course_1)
+session.commit()
+
+print(course_1)
+
+homework_1 = Homework(number=1, description='простая домашняя работа', course=course_1)
+
+homework_2 = Homework(number=2, description='сложная домашняя работа', course=course_1)
+session.add_all([homework_1, homework_2])
+session.commit()
+
+for homework in session.query(Homework).filter(Homework.description.like('%сложн%')).all():
+    print(homework)
+
+for course in session.query(Course).join(Homework.course).filter(Homework.number == 1).all():
+    print(course)
+
+course_2 = Course(name='Java')
+session.add(course_2)
+session.commit()
+
+subquery = session.query(Homework).filter(Homework.description.like('%сложн%')).subquery()
+for course in session.query(Course).join(subquery, Course.id == subquery.c.course_id):
+    print(course)
+
+session.query(Course).filter(Course.name == 'Java').update({'name': 'JavaScript'})
+session.commit()
+
+session.query(Course).filter(Course.name == 'JavaScript').delete()
+session.commit()
+
+for course in session.query(Course).all():
+    print(course)
 
 session.close()
